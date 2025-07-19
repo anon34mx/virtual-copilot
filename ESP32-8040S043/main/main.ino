457dd631486a0f64639a565b972e28066336d348
aaron <68936852+anon34mx@users.noreply.github.com>
2025-07-19 02:06:19 -0600

    Update main.ino
    bmp render function from MCU library

--- ESP32-8040S043/main/main.ino
+++ ESP32-8040S043/main/main.ino
@@ -82,9 +82,9 @@ float cX=0, cY=0, cAcc=0.03, cVel=0;
 // Adafruit_Image img;
 // const uint16_t bgImage_cursor [] PROGMEM;
 
-// images/bliss.bmp - 24bit
-// images/arrow.png
-// images/fondo.jpg
+// /images/bliss.bmp - 24bit
+// /images/arrow.png
+// /images/fondo.jpg
 static LGFX_Sprite background(&tft);
 
 void setup(void) {
@@ -95,7 +95,6 @@ void setup(void) {
   tft.setBrightness(255); 
   tft.setColorDepth(24);
 
-  tft.fillRect(10,10,34,34, TFT_GREEN);
   tft.fillScreen(TFT_DARKGREY);
 
 
@@ -122,7 +121,7 @@ void setup(void) {
   }
 
 	canvas.createSprite(20, 20);
-
+  showBMP("images/bliss.bmp", 100, 100);
 }
 
 
@@ -148,4 +147,161 @@ void loop(){
     xrec=0;
   }
   // deltaTime=currentTime;
+}
+
+
+// BMP IMAGES
+#define BUFFPIXEL      20
+#define PALETTEDEPTH   0
+uint16_t read16(File& f) {
+    uint16_t result;         // read little-endian
+    f.read((uint8_t*)&result, sizeof(result));
+    return result;
+}
+uint32_t read32(File& f) {
+    uint32_t result;
+    f.read((uint8_t*)&result, sizeof(result));
+    return result;
+}
+uint8_t showBMP(char *nm, int x, int y)
+{
+    File bmpFile;
+    int bmpWidth, bmpHeight;    // W+H in pixels
+    uint8_t bmpDepth;           // Bit depth (currently must be 24, 16, 8, 4, 1)
+    uint32_t bmpImageoffset;    // Start of image data in file
+    uint32_t rowSize;           // Not always = bmpWidth; may have padding
+    uint8_t sdbuffer[3 * BUFFPIXEL];    // pixel in buffer (R+G+B per pixel)
+    uint16_t lcdbuffer[(1 << PALETTEDEPTH) + BUFFPIXEL], *palette = NULL;
+    uint8_t bitmask, bitshift;
+    boolean flip = true;        // BMP is stored bottom-to-top
+    int w, h, row, col, lcdbufsiz = (1 << PALETTEDEPTH) + BUFFPIXEL, buffidx;
+    uint32_t pos;               // seek position
+    boolean is565 = false;      //
+
+    uint16_t bmpID;
+    uint16_t n;                 // blocks read
+    uint8_t ret;
+
+    if ((x >= tft.width()) || (y >= tft.height()))
+        return 1;               // off screen
+
+    bmpFile = SD.open(nm);      // Parse BMP header
+    bmpID = read16(bmpFile);    // BMP signature
+    (void) read32(bmpFile);     // Read & ignore file size
+    (void) read32(bmpFile);     // Read & ignore creator bytes
+    bmpImageoffset = read32(bmpFile);       // Start of image data
+    (void) read32(bmpFile);     // Read & ignore DIB header size
+    bmpWidth = read32(bmpFile);
+    bmpHeight = read32(bmpFile);
+    n = read16(bmpFile);        // # planes -- must be '1'
+    bmpDepth = read16(bmpFile); // bits per pixel
+    pos = read32(bmpFile);      // format
+    if (bmpID != 0x4D42) ret = 2; // bad ID
+    else if (n != 1) ret = 3;   // too many planes
+    else if (pos != 0 && pos != 3) ret = 4; // format: 0 = uncompressed, 3 = 565
+    else if (bmpDepth < 16 && bmpDepth > PALETTEDEPTH) ret = 5; // palette 
+    else {
+        bool first = true;
+        is565 = (pos == 3);               // ?already in 16-bit format
+        // BMP rows are padded (if needed) to 4-byte boundary
+        rowSize = (bmpWidth * bmpDepth / 8 + 3) & ~3;
+        if (bmpHeight < 0) {              // If negative, image is in top-down order.
+            bmpHeight = -bmpHeight;
+            flip = false;
+        }
+
+        w = bmpWidth;
+        h = bmpHeight;
+        if ((x + w) >= tft.width())       // Crop area to be loaded
+            w = tft.width() - x;
+        if ((y + h) >= tft.height())      //
+            h = tft.height() - y;
+
+        if (bmpDepth <= PALETTEDEPTH) {   // these modes have separate palette
+            //bmpFile.seek(BMPIMAGEOFFSET); //palette is always @ 54
+            bmpFile.seek(bmpImageoffset - (4<<bmpDepth)); //54 for regular, diff for colorsimportant
+            bitmask = 0xFF;
+            if (bmpDepth < 8)
+                bitmask >>= bmpDepth;
+            bitshift = 8 - bmpDepth;
+            n = 1 << bmpDepth;
+            lcdbufsiz -= n;
+            palette = lcdbuffer + lcdbufsiz;
+            for (col = 0; col < n; col++) {
+                pos = read32(bmpFile);    //map palette to 5-6-5
+                palette[col] = ((pos & 0x0000F8) >> 3) | ((pos & 0x00FC00) >> 5) | ((pos & 0xF80000) >> 8);
+            }
+        }
+
+        // Set TFT address window to clipped image bounds
+        tft.setAddrWindow(x, y, x + w - 1, y + h - 1);
+        for (row = 0; row < h; row++) { // For each scanline...
+            // Seek to start of scan line.  It might seem labor-
+            // intensive to be doing this on every line, but this
+            // method covers a lot of gritty details like cropping
+            // and scanline padding.  Also, the seek only takes
+            // place if the file position actually needs to change
+            // (avoids a lot of cluster math in SD library).
+            uint8_t r, g, b, *sdptr;
+            int lcdidx, lcdleft;
+            if (flip)   // Bitmap is stored bottom-to-top order (normal BMP)
+                pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+            else        // Bitmap is stored top-to-bottom
+                pos = bmpImageoffset + row * rowSize;
+            if (bmpFile.position() != pos) { // Need seek?
+                bmpFile.seek(pos);
+                buffidx = sizeof(sdbuffer); // Force buffer reload
+            }
+
+            for (col = 0; col < w; ) {  //pixels in row
+                lcdleft = w - col;
+                if (lcdleft > lcdbufsiz) lcdleft = lcdbufsiz;
+                for (lcdidx = 0; lcdidx < lcdleft; lcdidx++) { // buffer at a time
+                    uint16_t color;
+                    // Time to read more pixel data?
+                    if (buffidx >= sizeof(sdbuffer)) { // Indeed
+                        bmpFile.read(sdbuffer, sizeof(sdbuffer));
+                        buffidx = 0; // Set index to beginning
+                        r = 0;
+                    }
+                    switch (bmpDepth) {          // Convert pixel from BMP to TFT format
+                        case 32:
+                        case 24:
+                            b = sdbuffer[buffidx++];
+                            g = sdbuffer[buffidx++];
+                            r = sdbuffer[buffidx++];
+                            if (bmpDepth == 32) buffidx++; //ignore ALPHA
+                            color = tft.color565(r, g, b);
+                            break;
+                        case 16:
+                            b = sdbuffer[buffidx++];
+                            r = sdbuffer[buffidx++];
+                            if (is565)
+                                color = (r << 8) | (b);
+                            else
+                                color = (r << 9) | ((b & 0xE0) << 1) | (b & 0x1F);
+                            break;
+                        case 1:
+                        case 4:
+                        case 8:
+                            if (r == 0)
+                                b = sdbuffer[buffidx++], r = 8;
+                            color = palette[(b >> bitshift) & bitmask];
+                            r -= bmpDepth;
+                            b <<= bmpDepth;
+                            break;
+                    }
+                    lcdbuffer[lcdidx] = color;
+
+                }
+                tft.pushColors(lcdbuffer, lcdidx, first);
+                first = false;
+                col += lcdidx;
+            }           // end cols
+        }               // end rows
+        tft.setAddrWindow(0, 0, tft.width() - 1, tft.height() - 1); //restore full screen
+        ret = 0;        // good render
+    }
+    bmpFile.close();
+    return (ret);
 }
